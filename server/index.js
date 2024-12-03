232562763864f8e3ae1e3ce89d5c76af8f8177c9
user-irfan <kuttappuirfan244@gmail.com>
2024-12-03 17:04:43 +0530

    MVC architecture added for server

--- server/index.js
+++ server/index.js
@@ -1,19 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 const app = express();
 
-dotenv.config();
 
+dotenv.config();
 app.use(express.json());
+app.use(cors());
 
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log("Server is running on port 3001");
+    console.log("Server is running on port 3000");
 })
