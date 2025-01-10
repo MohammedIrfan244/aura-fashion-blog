c53aed1944f154b9f0d6c8179503f602833eb114
user-irfan <kuttappuirfan244@gmail.com>
2025-01-10 12:22:31 +0530

    feat: add cookie parser and error management middleware

--- server/index.js
+++ server/index.js
@@ -1,13 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import errorManager from "./middlewares/manageError";
 
 const app = express();
 
 
 dotenv.config();
+app.use(cookieParser());
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true
+}));
 
 
 app.get("/", (req, res) => {
@@ -16,6 +22,7 @@ app.get("/", (req, res) => {
 
 const port = process.env.PORT || 3000;
 
+app.use(errorManager)
 app.listen(port, () => {
     console.log("Server is running on port 3000");
 })
